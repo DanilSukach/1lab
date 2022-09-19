adfcf2cd6a11c4257b9558e0a873ad3390a478be
DanilSukach <danil211-1@yandex.ru>
2022-09-19 14:21:28 +0400

    Cкачивание и добавление в папку первых изображений

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ url='https://yandex.ru/images/search?text=tiger&lr=29388'
 resp=requests.get(url).text
 soup=BeautifulSoup(resp,'lxml')
 resp1=soup.find_all("div",class_="serp-item")
-b=1
+index=1
 for i in resp1:
     url_img=i.find("img",class_="serp-item__thumb justifier__thumb").get("src")
     print('https:' + url_img)
-    
     img=requests.get('https:' + url_img).content
-    a=str(b)
+    a=str(index)
     ul='tiger/' + a + '.jpg'
     print(ul)
     if not os.path.isdir("tiger"):
         os.mkdir("tiger")
     with open(ul,'wb') as handler:
         handler.write(img)
-    b=b+1
+    index+=1
